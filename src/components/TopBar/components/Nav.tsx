bf0a5f552ec2ab2baa14cfa412ab731df355adcf
jxde <root@localhost>
2020-10-12 02:50:19 +0300

    links & docs update

--- src/components/TopBar/components/Nav.tsx
+++ src/components/TopBar/components/Nav.tsx
@@ -5,11 +5,12 @@ import { NavLink } from 'react-router-dom'
 const Nav: React.FC = () => {
   return (
     <StyledNav>
-      <StyledLink exact activeClassName="active" to="/">Home</StyledLink>
-      <StyledLink exact activeClassName="active" to="/farm">Farm</StyledLink>
-      <StyledLink exact activeClassName="active" to="/migrate">Migrate</StyledLink>
-      <StyledLink exact activeClassName="active" to="/governance">Govern</StyledLink>
-      <StyledLink activeClassName="active" to="/faq">FAQ</StyledLink>
+      <StyledRouterLink exact activeClassName="active" to="/">Home</StyledRouterLink>
+      <StyledRouterLink exact activeClassName="active" to="/farm">Farm</StyledRouterLink>
+      <StyledRouterLink exact activeClassName="active" to="/migrate">Migrate</StyledRouterLink>
+      <StyledRouterLink exact activeClassName="active" to="/governance">Govern</StyledRouterLink>
+      <StyledRouterLink activeClassName="active" to="/faq">FAQ</StyledRouterLink>
+      <StyledLink href="https://yam.gitbook.io/yam/" target="_blank">Docs</StyledLink>
     </StyledNav>
   )
 }
@@ -19,7 +20,7 @@ const StyledNav = styled.nav`
   display: flex;
 `
 
-const StyledLink = styled(NavLink)`
+const StyledRouterLink = styled(NavLink)`
   color: ${props => props.theme.colors.grey[500]};
   font-weight: 700;
   padding-left: ${props => props.theme.spacing[3]}px;
@@ -30,6 +31,17 @@ const StyledLink = styled(NavLink)`
   }
   &.active {
     color: ${props => props.theme.colors.primary.main};
+  }
+`
+
+const StyledLink = styled.a`
+  color: ${props => props.theme.colors.grey[500]};
+  padding-left: ${props => props.theme.spacing[3]}px;
+  padding-right: ${props => props.theme.spacing[3]}px;
+  text-decoration: none;
+  font-weight: bold;
+  &:hover {
+    color: ${props => props.theme.colors.grey[600]};
   }
 `
 
