e957166be2f66a344b2c80466831f754934a217a
saconnolly <shaun.austin.connolly@gmail.com>
2020-10-27 17:35:08 -0400

    Clicking logo now navs to home (#21)
    Co-authored-by: shaun connolly <shaun.connolly@philips.com>

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import strainLogo from '../../assets/StrainNFT-Logo.png'
+import { useHistory  } from 'react-router-dom'
 
 const Logo: React.FC = () => {
-    return <img src={strainLogo} alt="Logo" height={48}/>;
+    let history = useHistory();
+
+    function imageClick() {
+        history.push('/');
+    }
+
+    return <img src={strainLogo} alt="Logo" height={48} onClick={() => imageClick()}/>;
 };
 
 export default Logo
