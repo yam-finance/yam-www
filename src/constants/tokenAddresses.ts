1618efb11b9285065c0dcfa05e47d88996cbba86
Kiko <ktimam@gmail.com>
2020-10-28 22:31:01 +0200

    Testnet contract addresses update.

--- src/constants/tokenAddresses.ts
+++ src/constants/tokenAddresses.ts
@@ -4,8 +4,8 @@ export const yamv3 = '0x0AaCfbeC6a24756c20D41914F2caba817C0d8521'
 export const yUsd = '0x5dbcf33d8c2e976c6b560249878e6f1491bca25c'
 export const yycrvUniLp = '0xb93Cc05334093c6B3b8Bfd29933bb8d5C031caBC'
 export const migrator = '0x72cfed9293cbfb2bfc7515c413048c697c6c811c'
-export const strn = '0x40fDD7f354aCB16a8A0Ba39d15EAc982bCa7b2eb'
-export const strnEthLP = '0x96e61f99c7d377f69d8f3766cb430565bd6fcf75'
-export const strnIncentivizer = '0x6853a4b89C5b0a771a5bD599741eAEE42FA6663F'
+export const strn = '0xDfc59572746FA03Ed8915E9023ebCbc62cd47c59'
+export const strnEthLP = '0x27623b0a2e50ff1598dc936ec52895d2f0a27131'
+export const strnIncentivizer = '0x94530a4995bf647f267D3c6c4846161b0eC15F80'
 // src/yam-sdk/lib/clean_build/contracts/STRNIncentivizer.json
-// make sure chainId is "1" and not "4" src/App.tsx
+// make sure chainId is "1" and not "4"
