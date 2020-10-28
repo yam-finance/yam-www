c0ab7f11e0be7151bee5ea53b42d06420194da44
Kiko <ktimam@gmail.com>
2020-10-29 00:04:48 +0200

    addresses update

--- src/constants/tokenAddresses.ts
+++ src/constants/tokenAddresses.ts
@@ -4,8 +4,8 @@ export const yamv3 = '0x0AaCfbeC6a24756c20D41914F2caba817C0d8521'
 export const yUsd = '0x5dbcf33d8c2e976c6b560249878e6f1491bca25c'
 export const yycrvUniLp = '0xb93Cc05334093c6B3b8Bfd29933bb8d5C031caBC'
 export const migrator = '0x72cfed9293cbfb2bfc7515c413048c697c6c811c'
-export const strn = '0xDfc59572746FA03Ed8915E9023ebCbc62cd47c59'
-export const strnEthLP = '0x27623b0a2e50ff1598dc936ec52895d2f0a27131'
-export const strnIncentivizer = '0x94530a4995bf647f267D3c6c4846161b0eC15F80'
+export const strn = '0x710CCAA8748dF2cf2E60c1633C9f17D302083e09'
+export const strnEthLP = '0x4de1c71fef9e2c9782e6645f04802bd75c83bdf0'
+export const strnIncentivizer = '0xaFf00B64CBCAAfC1bE4A45361326390c6F045c7B'
 // src/yam-sdk/lib/clean_build/contracts/STRNIncentivizer.json
 // make sure chainId is "1" and not "4"
