de5de0d2c8e0e84aba1591eb4e7d868079d12229
jamesbarnes97 <thomasthetrainrules-yyy@yahoo.com>
2020-10-29 03:20:37 +0000

    update lp and strn to mainnet addresses

--- src/constants/tokenAddresses.ts
+++ src/constants/tokenAddresses.ts
@@ -4,8 +4,8 @@ export const yamv3 = '0x0AaCfbeC6a24756c20D41914F2caba817C0d8521'
 export const yUsd = '0x5dbcf33d8c2e976c6b560249878e6f1491bca25c'
 export const yycrvUniLp = '0xb93Cc05334093c6B3b8Bfd29933bb8d5C031caBC'
 export const migrator = '0x72cfed9293cbfb2bfc7515c413048c697c6c811c'
-export const strn = '0x710CCAA8748dF2cf2E60c1633C9f17D302083e09'
-export const strnEthLP = '0x4de1c71fef9e2c9782e6645f04802bd75c83bdf0'
+export const strn = '0x90b426067be0b0ff5de257bc4dd6a4815ea03b5f'
+export const strnEthLP = '0xa198f36e3648dd16e75d721f0c6516e00e9ca053'
 export const strnIncentivizer = '0x19Bf9bef453f57983319Eb3033a95a7aa7DB764d'
 // src/yam-sdk/lib/clean_build/contracts/STRNIncentivizer.json
 // make sure chainId is "1" and not "4"
