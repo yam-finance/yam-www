bb8e6367d63ed4200d5ef0ebe103712f07f3fc3a
jamesbarnes97 <thomasthetrainrules-yyy@yahoo.com>
2021-02-13 18:15:15 +0000

    add comments to greenhouse context

--- src/contexts/Greenhouse/types.ts
+++ src/contexts/Greenhouse/types.ts
@@ -1,17 +1,17 @@
 export interface ContextValues {
-  parentOneNftId: string,
-  parentTwoNftId: string,
-  isBreeding: boolean,
-  onBreeding: () => void,
-  setBurnAmount: (amount: string) => void,
-  setStxpAmount: (amount: string) => void,
-  setParentOneNftId: (nftId: string) => void,
-  setParentTwoNftId: (nftId: string) => void,
-  burnAmount: string,
-  stxpAmount: string,
-  childName: string,
-  setChildName: (name: string) => void;
-  lpTokenAmount: string,
-  setLpTokenAmount: (amount: string) => void,
-  getBreedingFee: () => string,
+  onBreeding: () => void,           // method to call to breed the parents selected
+  isBreeding: boolean,              // indication that breading tx has been sent
+  setParentOneNftId: (nftId: string) => void,  // set first parent
+  parentOneNftId: string,           // set first parent's nft id
+  setParentTwoNftId: (nftId: string) => void,  // set second parent
+  parentTwoNftId: string,           // set second parent's nft id
+  setBurnAmount: (amount: string) => void,  // set the stxp amount to be burnt for breeding
+  burnAmount: string,               // stxp amount set
+  setStxpAmount: (amount: string) => void,  // set the stxp to give to child
+  stxpAmount: string,               // stxp amount to give child
+  setChildName: (name: string) => void;       // set the child name
+  childName: string,                // name of child nft
+  setLpTokenAmount: (amount: string) => void, // set the LP token amount
+  lpTokenAmount: string,            // amount of LP tokens to give child nft
+  getBreedingFee: () => string,     // get STRN fee after parents have been selected
 }
