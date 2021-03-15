42fb2c10bc7487b6e2067badb3353b8fceee0ca2
jason <kwon_jason@yahoo.com>
2021-03-16 01:35:02 +1100

    added register screen: /register

---
+++ src/views/Registration/components/RegisterNotice.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Notice, NoticeContent, NoticeIcon, Box, Spacer } from "react-neu";
+import styled from "styled-components";
+
+import useGovernance from 'hooks/useGovernance';
+
+const RegisterNotice: React.FC = () => {
+  const { isRegistered } = useGovernance();
+
+  return (
+    <Notice isHidden={isRegistered}>
+      <NoticeIcon>{isRegistered ? "✔️" : "🗣️"}</NoticeIcon>
+      <NoticeContent>
+        <StyledNoticeContentInner>
+          <span style={{ width: '100%', textAlign: 'center', fontSize: 20 }}>{isRegistered ? `You've successfuly registered to vote!` : `You must register to vote!`}</span>
+          <Spacer size="sm" />
+        </StyledNoticeContentInner>
+      </NoticeContent>
+    </Notice>
+  );
+};
+
+const StyledNoticeContentInner = styled.div`
+  align-items: center;
+  display: flex;
+  @media (max-width: 768px) {
+    flex-flow: column nowrap;
+    align-items: flex-start;
+  }
+`;
+
+export default RegisterNotice;
