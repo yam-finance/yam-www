47a90eee25e2fecfcb263433f56e0d1c06819304
ethedev <2432272+ethedev@users.noreply.github.com>
2021-03-22 18:34:57 +0400

    update registeration

--- src/views/Registration/components/RegisterNotice.tsx
+++ src/views/Registration/components/RegisterNotice.tsx
@@ -12,7 +12,7 @@ const RegisterNotice: React.FC = () => {
       <NoticeIcon>{isRegistered ? "✔️" : "🗣️"}</NoticeIcon>
       <NoticeContent>
         <StyledNoticeContentInner>
-          <span style={{ width: '100%', textAlign: 'center', fontSize: 20 }}>{isRegistered ? `You've successfuly registered to vote!` : `You must register to vote!`}</span>
+          <span style={{ width: '100%', textAlign: 'center', fontSize: 20 }}>{isRegistered ? `You've successfuly registered!` : `It's time to register to vote for onchain and Snapshot proposals.`}</span>
           <Spacer size="sm" />
         </StyledNoticeContentInner>
       </NoticeContent>
