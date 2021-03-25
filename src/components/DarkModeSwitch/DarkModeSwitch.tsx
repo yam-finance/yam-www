6b75246feafa213716cb1d94916d30650ddd6e7a
jason <kwon_jason@yahoo.com>
2021-03-25 17:25:26 +1100

    active color on pagination dark theme

--- src/components/DarkModeSwitch/DarkModeSwitch.tsx
+++ src/components/DarkModeSwitch/DarkModeSwitch.tsx
@@ -12,9 +12,11 @@ const DarkModeSwitch: React.FC = () => {
     if (darkMode) {
       document.documentElement.style.setProperty("--gridline-drop-shadow", "drop-shadow( 0px 1px 0px rgba(240, 240, 240, 0.13))");
       document.documentElement.style.setProperty("--pagination-text-color", "white");
+      document.documentElement.style.setProperty("--pagination-background-color", "rgba(45, 32, 36, 0.35)");
     } else {
       document.documentElement.style.setProperty("--gridline-drop-shadow", "drop-shadow(0 -1px 0px hsl(338deg 20% 90% / 100%))");
       document.documentElement.style.setProperty("--pagination-text-color", "black");
+      document.documentElement.style.setProperty("--pagination-background-color", "rgba(0, 0, 0, 0.05)");
     }
     setDarkModeSetting(darkMode);
   }, [darkMode, setDarkModeSetting]);
