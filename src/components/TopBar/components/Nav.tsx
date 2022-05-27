d1b02bd17f529e5265deef9783372c9916410581
Ross <therossgalloway@gmail.com>
2022-05-27 13:10:33 -0400

    website cleanup

--- src/components/TopBar/components/Nav.tsx
+++ src/components/TopBar/components/Nav.tsx
@@ -16,22 +16,11 @@ const Nav: React.FC<NavProps> = ({ onDismiss, mobileMenu }) => {
       )}
       <StyledNav>
         <StyledRouterLink target="dashboard" label="Dashboard" mobileMenu={mobileMenu} onDismiss={onDismiss} />
-        <StyledRouterLink target="projects" label="Projects" mobileMenu={mobileMenu} onDismiss={onDismiss}>
-          <StyledLink href="http://synths.yam.xyz" label="Yam Synths" mobileMenu={mobileMenu} onDismiss={onDismiss} />
-          <StyledRouterLink target="umbrella" label="Yam Protection" mobileMenu={mobileMenu} onDismiss={onDismiss} />
-          <StyledRouterLink target="daohouse" label="Yam Treasuries" mobileMenu={mobileMenu} onDismiss={onDismiss} />
-          <StyledLink href="https://nft.yam.xyz/" label="MOFY" mobileMenu={mobileMenu} onDismiss={onDismiss} />
-        </StyledRouterLink>
-        <StyledLink href="http://synths.yam.xyz" label="Synths" mobileMenu={mobileMenu} onDismiss={onDismiss} />
-        <StyledRouterLink target="governance" label="Govern" mobileMenu={mobileMenu} onDismiss={onDismiss} >
-          <StyledRouterLink target="register" label="Register" mobileMenu={mobileMenu} onDismiss={onDismiss} />
-          <StyledRouterLink target="delegate" label="Delegate" mobileMenu={mobileMenu} onDismiss={onDismiss} />
-        </StyledRouterLink>
-        <StyledRouterLink target="farm" label="Farm" mobileMenu={mobileMenu} onDismiss={onDismiss} >
-          <StyledRouterLink target="claim" label="Claim" mobileMenu={mobileMenu} onDismiss={onDismiss} />
-        </StyledRouterLink>
-        <StyledRouterLink target="/tvl" label="TVL" mobileMenu={mobileMenu} onDismiss={onDismiss} />
-        <StyledLink href="https://docs.yam.finance/" label="FAQ" mobileMenu={mobileMenu} onDismiss={onDismiss} />
+        <StyledRouterLink target="projects" label="Projects" mobileMenu={mobileMenu} onDismiss={onDismiss} />
+        <StyledRouterLink target="governance" label="Govern" mobileMenu={mobileMenu} onDismiss={onDismiss} />
+        <StyledRouterLink target="farm" label="Farm" mobileMenu={mobileMenu} onDismiss={onDismiss} />
+        <StyledRouterLink target="Migrate" label="Migrate to V3" mobileMenu={mobileMenu} onDismiss={onDismiss} />
+        <StyledLink href="https://docs.yam.finance/" label="FAQ↗" mobileMenu={mobileMenu} onDismiss={onDismiss} />
       </StyledNav>
     </>
   );
