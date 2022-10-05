e9bcd90b7f41ad3d03fee04ce16d8e7e044db190
ethedev <2432272+ethedev@users.noreply.github.com>
2022-10-05 05:27:04 +0400

    SDK integration

--- src/hooks/useSDK.ts
+++ src/hooks/useSDK.ts
@@ -1,8 +1,7 @@
 import { ethers } from "ethers";
 import { useCallback, useEffect, useState } from "react";
 import { useWallet } from "use-wallet";
-import Yam from "yam-sdk-files/dist";
-// import Yam from "yam-sdk";
+import Yam from "yam-sdk";
 
 const useSDK = () => {
   const { ethereum } = useWallet();
